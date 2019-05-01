8116b04b5df90687676e0474e70f55931f32075a
AlbAngs <42752130+AlbAngs@users.noreply.github.com>
2019-05-01 14:46:51 -0500

    mi primer commit

---
+++ application/views/hf/pie.php
@@ -0,0 +1,20 @@
+	<div class="footer">
+			<hr>
+		<hr class="invisible">
+		<div class="text-center">
+			Formulación y Evaluación de proyectos Copyright &#169 Derechos Reservados 2019-FEP 
+		</div>
+		<hr class="invisible">
+	</div>
+
+</body>
+
+
+
+<script type="text/javascript">
+
+  $(window).ready(function(){
+    $(<?php if(isset($modal)) echo "'#".$modal."'";?>).modal("show");
+  });
+
+</script>
